82c3a900826872cbb641fc1994212b10181f6617
unknown <erdalcodes@gmail.com>
2023-03-26 13:45:50 +0300

    footer design

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,61 @@
 import React from "react";
 import styles from "@/styles/footer.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <nav>
-        <ul>
-          <li>Foods</li>
-          <li>Drinks</li>
-          <li>About</li>
-          <li>Contact</li>
+      <div className="logo">
+        <Link href="/">
+          <Image
+            src="/images/logo2.png"
+            width={120}
+            height={90}
+            alt="Shop logo"
+          />
+        </Link>
+      </div>
+      <nav className={styles.nav}>
+        <ul className={styles.ul}>
+          <li>
+            <Link href="/foods" className={styles.link}>
+              Foods
+            </Link>
+          </li>
+          <li>
+            <Link href="/foods" className={styles.link}>
+              Drinks
+            </Link>
+          </li>
+          <li>
+            <Link href="/foods" className={styles.link}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/foods" className={styles.link}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+
+        <ul className={styles.ul}>
+          <li>
+            <Link href="/privacy" className={styles.link}>
+              Privacy
+            </Link>
+          </li>
+          <li>
+            <Link href="/calories" className={styles.link}>
+              Calories
+            </Link>
+          </li>
+          <li>
+            <Link href="/faq" className={styles.link}>
+              FAQs
+            </Link>
+          </li>
         </ul>
       </nav>
     </footer>
